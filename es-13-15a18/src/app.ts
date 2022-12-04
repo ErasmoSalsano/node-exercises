3078b8e24f463d14aebcbb041875b8b395a31295
Erasmo Salsano <erasmosalsano@gmail.com>
2022-12-04 02:11:48 +0100

    Add es-18

--- es-13-15a18/src/app.ts
+++ es-13-15a18/src/app.ts
@@ -14,16 +14,36 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   const body = req.body;
-  if (typeof body.data.text != "string") {
+  if (typeof body.text != "string") {
     res.status(400);
-
     res.json({ message: "Invalid data type" });
     return;
   }
 
-  body.data.length = body.data.text.length;
-  body.data.readingTimeM = Math.floor(body.data.length / 20);
-  res.json(await prisma.blabbering.create(body));
+  body.length = body.text.length;
+  body.readingTimeM = Math.floor(body.length / 20);
+  res.json(await prisma.blabbering.create({ data: body }));
+});
+
+app.put("/:id", async (req, res) => {
+  const body = req.body;
+  const { id } = req.params;
+  console.log(id);
+  if (typeof body.text != "string") {
+    res.status(400);
+    res.json({ message: "Invalid data type" });
+    return;
+  }
+  body.length = body.text.length;
+  body.readingTimeM = Math.floor(body.length / 20);
+  res.json(
+    await prisma.blabbering.update({
+      where: {
+        id: Number(id),
+      },
+      data: body,
+    })
+  );
 });
 
 app.get("/:id", async (req, res) => {
