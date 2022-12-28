42223827cf5f1139c0f6137d7727475a94cd02a9
Erasmo Salsano <erasmosalsano@gmail.com>
2022-12-28 03:33:39 +0100

    Move all configurations into one script (config.ts)

--- es-16+/src/server.ts
+++ es-16+/src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "./app";
 import "dotenv/config";
+import config from "./config";
 
-const port = process.env.PORT;
+const port = config.PORT;
 
 app.listen(port, () => console.log(`running at localhost:${port}`));
